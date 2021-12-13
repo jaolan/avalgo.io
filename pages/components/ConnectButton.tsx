1dcf3c3c277ed976ce332293a9d0023fe117241b
jaolan <olanjorel@gmail.com>
2021-12-12 22:08:02 -0600

    Add logout functionality to ConnectButton

--- pages/components/ConnectButton.tsx
+++ pages/components/ConnectButton.tsx
@@ -2,15 +2,23 @@ import { useMoralis } from "react-moralis";
 import { Web3Provider } from "react-moralis/lib/hooks/core/useMoralis/_useMoralisWeb3";
 
 export default function ConnectButton() {
-	const { authenticate } = useMoralis()
+	const { authenticate, isAuthenticated, logout } = useMoralis()
 
 	return(
-		<button
-			onClick={() => {
-				authenticate({ provider: "metamask" as Web3Provider});
-			}}
-		>
-			Sign in with MetaMask
-		</button>
+		<div>
+      {isAuthenticated ? (
+        <>
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <button
+          onClick={() => {
+            authenticate({ provider: "metamask" as Web3Provider});
+          }}
+        >
+          Sign in with MetaMask
+        </button>
+      )}
+    </div>
 	)
 }
