a1582a80aa42ce4e17e886018b5b4788f5fb95da
jaolan <olanjorel@gmail.com>
2021-12-14 11:18:04 -0600

    Add onClick actions to submit code, add CodeView css

--- pages/code.tsx
+++ pages/code.tsx
@@ -14,7 +14,12 @@ const code: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <h1 className={styles.title}>Code</h1>
+      <p className={styles.description}>Get the kth largest subarray for the following: [1,2,3,4,5]</p>
+      <div className="absolute top-20 bottom-40 left-10 right-10">
       <CodeView />
+      </div>
+      <p>Bounty: 0.04 AVAX</p>
     </div>
 
 
--- pages/components/CodeView.tsx
+++ pages/components/CodeView.tsx
@@ -1,21 +1,38 @@
+import { useState } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
-
-// Custom theme needs to be declared and imported for CodeMirror.
 import { oneDark }from './oneDarkTheme';
 
-const code = 'console.log(\'hello world!\');'
+import styles from '/styles/CodeView.module.css'
+
 
 export default function CodeView() {
+  // set states for UI
+  const [code, setCode] = useState('console.log(\'hello world!\');')
+
+  // submit code to 'backend'
+  const submitCode = () => {
+    console.log(code)
+  }
+
   return (
-    <CodeMirror
-    value={code}
-    height="200px"
-    theme="dark"
-    extensions={[javascript({ jsx: true }), oneDark]}
-    onChange={(value, viewUpdate) => {
-      console.log('value:', value);
-    }}
-    />
+    <div>
+      <CodeMirror
+      value={code}
+      height="200px"
+      theme="dark"
+      extensions={[javascript({ jsx: true }), oneDark]}
+      onChange={(value, viewUpdate) => {
+        setCode(value)
+        // console.log('value:', code)
+      }}
+      />
+      <button className={styles.submitBtn} onClick={submitCode}>
+        Submit Code
+      </button>
+      <button className={styles.submitBtn} onClick={submitCode}>
+        Run Tests
+      </button>
+    </div>
   );
 }
