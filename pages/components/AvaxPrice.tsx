c9fc24e5b15662d7d5546a32ec1e9361bdc9b73e
jaolan <olanjorel@gmail.com>
2022-01-01 12:57:42 -0500

    Update AVAX price feed to refresh on timer

--- pages/components/AvaxPrice.tsx
+++ pages/components/AvaxPrice.tsx
@@ -2,13 +2,25 @@ import React, { useEffect, useState } from "react";
 import { useMoralisWeb3Api, useMoralisWeb3ApiCall, useWeb3ExecuteFunction } from "react-moralis";
 import MoralisType, { Moralis } from 'moralis';
 
-function AvaxPrice () {
-
-  // set state variable for AVAX price
+const AvaxPrice = () => {
   const [price, setPrice] = useState(0)
+  // time interval for refreshing price component
+  const [count, setCount] = React.useState(0);
 
   // set states for UI
   // const [code, setCode] = useState<string>(userCode + testCode)
+
+  // Fetch AVAX price from contract every X seconds
+  useEffect(() => {
+    const timer = window.setInterval(() => {
+      setCount(count => count + 1)
+      handlePrice()
+    }, 10000);
+
+    return () => {
+      window.clearInterval(timer);
+    };
+  }, []);
 
   // Set smartcontract ABI - This will allow us to call chainlink price getter
   const ABI = [
@@ -32,6 +44,8 @@ function AvaxPrice () {
     }
   ]
 
+  // interact w/ chainlink contract we deployed to track AVAX price
+  //  contract addy: 0x8705A5883B520a672fB89865ed8Afa66828f3987
   const handlePrice = async () => {
     try {
       const options = {
@@ -40,9 +54,11 @@ function AvaxPrice () {
         function_name: "getLatestPrice",
         abi: ABI
       };
+      // The below moralis func will NOT run in typescript as of 1/1/2022, use ignore
       // @ts-ignore
       const price = await Moralis.Web3API.native.runContractFunction(options);
-      const fmtPrice: number = Number(price) * 10**-8
+      // writing numbers in javascript is fun
+      const fmtPrice: number = parseFloat((Number(price) * 10**-8).toFixed(2))
       console.log(fmtPrice);
       setPrice(fmtPrice)
       return true
@@ -55,7 +71,7 @@ function AvaxPrice () {
 
   return (
     <div>
-      {<button onClick={handlePrice}>Avax price (click to refresh): {price}</button>}
+      <p>AVAX Price: {price}</p>
     </div>
   )
 }
